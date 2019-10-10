3678b804f3926dbb6f53217eba2d5136b1ef8d1c
affandzakyafif <affandzaky@gmail.com>
2019-10-10 21:24:34 +0700

    while

---
+++ latihan.2/latihan-3.js
@@ -0,0 +1,10 @@
+for ( j = 1; j<18; j += 2){
+    document.write(j + "<br>")
+}
+    var nilai = "";
+    var j = 1;
+    while (j < 18){ 
+        nilai += j; j+=2;
+
+    }
+    document.write(nilai);
